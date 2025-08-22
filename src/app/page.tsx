43b452800052948a30af56ef447ec16b38cd5231
matthew-adel-vodafone <matthew.awad@vodafone.com>
2025-08-22 11:28:23 +0300

    finish the basic setup of our app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="hero text-red-600 bg-blue-100 p-4 mt-36">
-        hero - Tailwind is now working!
-      </div>
+      <h1 className="bg-red-700">hero -</h1>
     </main>
   );
 }
